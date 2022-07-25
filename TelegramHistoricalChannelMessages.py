3ec17415839a11b1e0ce4ce1c52530117b395d3d
PrateekMittal2001 <p.mittal1002@gmail.com>
2022-07-25 16:02:24 +0530

    config data configured

--- TelegramHistoricalChannelMessages.py
+++ TelegramHistoricalChannelMessages.py
@@ -1,14 +1,12 @@
 from telethon import TelegramClient
 from telethon.errors import SessionPasswordNeededError
 from telethon.tl.functions.messages import (GetHistoryRequest)
-from telethon.tl.types import (
-    PeerChannel
-)
+from telethon.tl.types import (PeerChannel)
 
 api_id = 17304508
 api_hash = "1fa688006105dd573df6be757cc4f722"
 
-# get the phone number and user name
+# get the phone number and username
 phone = +919717020263
 username = "@CoronaVirus1234"
 
